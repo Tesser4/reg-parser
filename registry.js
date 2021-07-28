f4cda43af19f8f0f9eb9c887e29d404892e8c5cb
Tesser4 <kostas@tux.gr>
2021-07-29 00:32:56 +0300

    feat: Tree structure draft implementation

---
+++ registry.js
@@ -0,0 +1,62 @@
+
+import factorizeNode from './node.js'
+import { parseKeyDataString } from './utils.js'
+
+let rootNode = null
+let numOfKeys = 0
+
+const getRoot = () => rootNode
+
+const init = () => {
+  rootNode = null
+  numOfKeys = 0
+}
+
+const getNumberOfKeys = () => numOfKeys
+
+const insertKey = (keyDataString) => {
+  const { keys, data } = parseKeyDataString(keyDataString)
+
+  if (rootNode && rootNode.getKeyName() !== keys[0])
+    throw new Error('Key to be inserted has invalid root key')
+
+  if (!rootNode && keys.length === 1) {
+    rootNode = factorizeNode(keys[0], data)
+    numOfKeys += 1
+    return
+  }
+
+  let currentNode
+  if (rootNode) {
+    currentNode = rootNode
+  } else {
+    currentNode = factorizeNode(keys[0])
+    numOfKeys += 1
+    rootNode = currentNode
+  }
+
+  for (let i = 0; i < keys.length - 1; i += 1) {
+    const nextIdx = i + 1
+    const nextKeyName = keys[nextIdx]
+    if (currentNode.hasChild(nextKeyName)) {
+      currentNode = currentNode.getChild(nextKeyName)
+    } else {
+      const isNextKeyLast = nextIdx === keys.length - 1
+      const nextNode = isNextKeyLast
+        ? factorizeNode(nextKeyName, data)
+        : factorizeNode(nextKeyName)
+      currentNode.addChild(nextNode)
+      numOfKeys += 1
+      currentNode = nextNode
+    }
+  }
+}
+
+const registry = {
+  getRoot,
+  init,
+  getNumberOfKeys,
+  insertKey,
+}
+
+export default registry
---
+++ registry.test.js
@@ -0,0 +1,94 @@
+
+import {
+  assertEquals,
+  assertThrows,
+} from 'https://deno.land/std@0.102.0/testing/asserts.ts'
+import registry from './registry.js'
+
+Deno.test('registry inserts a string key sequence with its data', () => {
+  registry.init()
+  const r = `[HKLM\\foo\\bar\\baz]\r\n"Par1"="val1"\r\n"Par2"="val2"`
+  registry.insertKey(r)
+  const root = registry.getRoot()
+  assertEquals(root.getKeyName(), 'HKLM')
+  assertEquals(root.getKeyData(), {})
+  assertEquals(root.hasChild('foo'), true)
+  assertEquals(root.hasChild('bar'), false)
+  let next = root.getChild('foo')
+  assertEquals(next.getKeyName(), 'foo')
+  assertEquals(next.getKeyData(), {})
+  assertEquals(next.hasChild('bar'), true)
+  next = next.getChild('bar')
+  next = next.getChild('baz')
+  assertEquals(next.getKeyName(), 'baz')
+  assertEquals(next.getKeyData(), {
+    Par1: "val1",
+    Par2: "val2",
+  })
+})
+
+Deno.test('registry inserts two string key sequences with data', () => {
+  registry.init()
+  const r1 = `[HKLM\\foo\\bar]\r\n"ApplicationRuntime"="true"\r\n"ApplicationTest"="false"`
+  const r2 = `[HKLM\\foo\\bar\\baz]\r\n"Par1"="val1"\r\n"Par2"="val2"`
+  registry.insertKey(r1)
+  registry.insertKey(r2)
+  const root = registry.getRoot()
+  assertEquals(root.getKeyName(), 'HKLM')
+  assertEquals(root.getKeyData(), {})
+  assertEquals(root.hasChild('foo'), true)
+  assertEquals(root.hasChild('bar'), false)
+  let next = root.getChild('foo')
+  assertEquals(next.getKeyName(), 'foo')
+  assertEquals(next.getKeyData(), {})
+  assertEquals(next.hasChild('bar'), true)
+  next = next.getChild('bar')
+  assertEquals(next.getKeyName(), 'bar')
+  assertEquals(next.getKeyData(), {
+    ApplicationRuntime: "true",
+    ApplicationTest: "false",
+  })
+  next = next.getChild('baz')
+  assertEquals(next.getKeyName(), 'baz')
+  assertEquals(next.getKeyData(), {
+    Par1: "val1",
+    Par2: "val2",
+  })
+})
+
+Deno.test('registry returns total number of keys', () => {
+  registry.init()
+  const r1 = `[HKLM\\foo\\bar]\r\n"Par1"="val1"\r\n"Par2"="val2"`
+  const r2 = `[HKLM\\foo\\bar\\baz]\r\n"Par3"="val3"\r\n"Par4"="val4"`
+  registry.insertKey(r1)
+  registry.insertKey(r2)
+  const expected = 4
+  const result = registry.getNumberOfKeys()
+  assertEquals(result, expected)
+})
+
+Deno.test('registry insertion can handle string with unary key and its data', () => {
+  registry.init()
+  const reg = `[HKLM]\r\n"foo"="bar"\r\n"baz"="bar-bar"`
+  registry.insertKey(reg)
+  const root = registry.getRoot()
+  assertEquals(root.getKeyName(), 'HKLM')
+  assertEquals(root.getKeyData(), {
+    foo: "bar",
+    baz: "bar-bar",
+  })
+})
+
+Deno.test('registry insertion throws for key with different root', () => {
+  const r1 = `[HKLM\\foo\\bar]\r\n"Par1"="val1"\r\n"Par2"="val2"`
+  const r2 = `[HKL\\foo\\bar\\baz]\r\n"Par3"="val3"\r\n"Par4"="val4"`
+  registry.init()
+  assertThrows(
+    () => {
+      registry.insertKey(r1)
+      registry.insertKey(r2)
+    },
+    Error,
+    'Key to be inserted has invalid root key'
+  )
+})
